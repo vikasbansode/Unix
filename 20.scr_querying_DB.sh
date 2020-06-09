ce874b6fce20fbb8c1cc9a49bcd14feae3f8413d
Vikas Hanamant Bansode <62126392+vikasbansode@users.noreply.github.com>
2020-06-09 07:39:28 +0530

    Update 20.scr_querying_DB.sh

--- 20.scr_querying_DB.sh
+++ 20.scr_querying_DB.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-###################################################
-#TASK : Querying Data from Database               #
-#Student : Vikas Bansode		                      # 	
-#Date : 10/20/2019				                        #	
-###################################################
+#################################################
+#TASK : Querying Data from Database             #
+#Student : Vikas Bansode		                    # 	
+#Date : 10/20/2019				                      #	
+#################################################
 
 sqlplus -s SYSTEM/vb_conn <<-EOF
 set underline off
